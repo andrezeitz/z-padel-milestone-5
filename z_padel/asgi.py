4cb6906b80b710480978e103028551e0646cbd83
andrezeitz <swe_zeitz@hotmail.com>
2021-12-09 12:30:22 +0000

    initial comit

---
+++ z_padel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for z_padel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'z_padel.settings')
+
+application = get_asgi_application()
---
+++ z_padel/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for z_padel project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'z_padel.settings')
+
+application = get_wsgi_application()
