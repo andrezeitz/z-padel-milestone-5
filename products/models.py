55bf9562bce4437b84c3ec99111960a5fdcafdf2
andrezeitz <swe_zeitz@hotmail.com>
2021-12-15 14:10:39 +0000

    Update all products category links

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    category_text = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
