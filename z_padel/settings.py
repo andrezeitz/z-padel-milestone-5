476563200278026bfb91f573b42379e858a74d37
andrezeitz <swe_zeitz@hotmail.com>
2022-01-03 12:50:11 +0000

    Removed secret key and set debug

--- z_padel/settings.py
+++ z_padel/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+
 ALLOWED_HOSTS = ['z-padel.herokuapp.com', 'localhost']
 
 
