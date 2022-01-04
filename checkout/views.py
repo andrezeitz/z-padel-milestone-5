1b1310fba5f723b443f496cb1c00ade5f28ef49d
andrezeitz <swe_zeitz@hotmail.com>
2022-01-04 14:48:55 +0000

    Added checkout success logic

--- checkout/views.py
+++ checkout/views.py
@@ -91,9 +91,6 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
