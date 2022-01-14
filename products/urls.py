14653b700b1b435b7fa4ad47e8575127ab682cde
andrezeitz <swe_zeitz@hotmail.com>
2022-01-14 12:42:46 +0000

    Create delete product view and add buttons in product template

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     # path('<selected_category>/<slug:slug>/', views.product_detail, name='product_detail'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
-    path('add/', views.add_product, name='add_product'),
-    path('edit/<product_id>/', views.edit_product, name='edit_product'),
-    path('delete/<product_id>/', views.delete_product, name='delete_product'),
+    path('<intadd/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
